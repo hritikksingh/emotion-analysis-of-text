28f294487397371c65fadc4fdb6a7ba9c5620621
HRITIK KUMAR SINGH <40272214+hritikpapa@users.noreply.github.com>
2021-05-10 15:40:56 +0530

    First commit

---
+++ clean_data.py
@@ -0,0 +1,122 @@
+
+import preprocessor as p
+import numpy as np 
+import pandas as pd 
+import emoji
+import keras
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers.recurrent import LSTM, GRU,SimpleRNN
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.embeddings import Embedding
+from keras.layers.normalization import BatchNormalization
+from keras.utils import np_utils
+from sklearn import preprocessing, decomposition, model_selection, metrics, pipeline
+from keras.layers import GlobalMaxPooling1D, Conv1D, MaxPooling1D, Flatten, Bidirectional, SpatialDropout1D
+from keras.preprocessing import sequence, text
+from keras.callbacks import EarlyStopping
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+import plotly.graph_objects as go
+import plotly.express as px
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import ModelCheckpoint
+import transformers
+from transformers import TFAutoModel, AutoTokenizer
+from tqdm.notebook import tqdm
+from tokenizers import Tokenizer, models, pre_tokenizers, decoders, processors
+from tqdm import tqdm
+
+
+# Misspelled data
+
+misspell_data = pd.read_csv("aspell.txt",sep=":",names=["correction","misspell"])
+misspell_data.misspell = misspell_data.misspell.str.strip()
+misspell_data.misspell = misspell_data.misspell.str.split(" ")
+misspell_data = misspell_data.explode("misspell").reset_index(drop=True)
+misspell_data.drop_duplicates("misspell",inplace=True)
+miss_corr = dict(zip(misspell_data.misspell, misspell_data.correction))
+
+
+def misspelled_correction(val):
+    for x in val.split(): 
+        if x in miss_corr.keys(): 
+            val = val.replace(x, miss_corr[x]) 
+    return val
+
+
+
+#Contractions
+
+contractions = pd.read_csv("contractions.csv")
+cont_dic = dict(zip(contractions.Contraction, contractions.Meaning))
+
+
+def cont_to_meaning(val): 
+  
+    for x in val.split(): 
+        if x in cont_dic.keys(): 
+            val = val.replace(x, cont_dic[x]) 
+    return val
+
+
+
+
+
+#Punctuations and emojis
+
+def punctuation(val): 
+  
+    punctuations = '''()-[]{};:'"\,<>./@#$%^&_~'''
+  
+    for x in val.lower(): 
+        if x in punctuations: 
+            val = val.replace(x, " ") 
+    return val
+
+
+
+
+def clean_text(val):
+    val = misspelled_correction(val)
+    val = cont_to_meaning(val)
+    val = p.clean(val)
+    val = ' '.join(punctuation(emoji.demojize(val)).split())
+    
+    return val
+
+
+
+#assigning id to each sentiment
+sent_to_id  = {"empty":0, "sadness":1,"enthusiasm":2,"neutral":3,"worry":4,
+                        "surprise":5,"love":6,"fun":7,"hate":8,"happiness":9,"boredom":10,"relief":11,"anger":12}
+
+
+
+
+
+import pickle
+token=pickle.load(open("token.pkl","rb"))
+max_len = 160
+
+
+def get_sentiment(model,text):
+    text = clean_text(text)
+    #tokenize
+    twt = token.texts_to_sequences([text])
+    twt = sequence.pad_sequences(twt, maxlen=max_len, dtype='int32')
+    sentiment = model.predict(twt,batch_size=1,verbose = 2)
+    sent = np.round(np.dot(sentiment,100).tolist(),0)[0]
+    result = pd.DataFrame([sent_to_id.keys(),sent]).T
+    result.columns = ["sentiment","percentage"]
+    result=result[result.percentage !=0]
+    return result
+
+
+
+
+
+
+
